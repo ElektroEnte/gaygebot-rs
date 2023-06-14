6118e2214678238b2802156d34d85ca00dd8696f
mzntori <44904735+ElektroEnte@users.noreply.github.com>
2023-06-14 17:09:45 +0200

    restructure project, add extra jobs

---
+++ src/bot/env/counter.rs
@@ -0,0 +1,38 @@
+#[derive(Copy, Clone, Debug, PartialOrd, PartialEq)]
+pub struct Counter {
+    value: i64,
+}
+
+impl Default for Counter {
+    fn default() -> Self {
+        Counter { value: 0 }
+    }
+}
+
+impl From<i64> for Counter {
+    fn from(i: i64) -> Self {
+        Counter { value: i }
+    }
+}
+
+impl Counter {
+    pub fn new(value: &impl Into<i64>) -> Self {
+        Counter { value: value.into() }
+    }
+
+    pub fn get(&self) -> i64 {
+        self.value
+    }
+
+    pub fn set(&mut self, value: i64) {
+        self.value = value
+    }
+
+    pub fn add(&mut self, value: i64) {
+        self.set(self.get() + value);
+    }
+
+    pub fn subtract(&mut self, value: i64) {
+        self.set(self.get() - value)
+    }
+}

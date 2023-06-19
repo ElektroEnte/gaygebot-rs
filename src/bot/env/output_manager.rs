0cfc247d199429e043846dbb60773510c970813d
mzntori <44904735+ElektroEnte@users.noreply.github.com>
2023-06-19 08:09:05 +0200

    add cache fir resent messages to go around twitch 30 second same message cooldown

--- src/bot/env/output_manager.rs
+++ src/bot/env/output_manager.rs
@@ -10,10 +10,26 @@ use twitch_irc::{SecureTCPTransport, TwitchIRCClient};
 use crate::bot::Output;
 use super::environment::BotPermissions;
 
+#[derive(Clone, Debug, PartialEq)]
+pub struct CacheItem {
+    text: String,
+    timestamp: Instant,
+}
+
+impl CacheItem {
+    pub fn new(text: String) -> Self {
+        CacheItem {
+            text,
+            timestamp: Instant::now(),
+        }
+    }
+}
+
 #[derive(Clone, Debug)]
 pub struct OutputQueue {
     queue: Vec<Output>,
     last_sent: Instant,
+    cache: Vec<CacheItem>,
     pub is_running: bool,
 }
 
@@ -22,6 +38,7 @@ impl OutputQueue {
         OutputQueue {
             queue: vec![],
             last_sent: Instant::now(),
+            cache: vec![],
             is_running: false,
         }
     }
@@ -34,25 +51,42 @@ impl OutputQueue {
         self.queue.len()
     }
     
-    pub async fn run(manager: Arc<Mutex<OutputQueue>>, client: Arc<Mutex<TwitchIRCClient<SecureTCPTransport, StaticLoginCredentials>>>) {
-        let mut locked_manager = manager.lock().await;
-        locked_manager.is_running = true;
-        drop(locked_manager);
+    pub async fn run(queue: Arc<Mutex<OutputQueue>>, client: Arc<Mutex<TwitchIRCClient<SecureTCPTransport, StaticLoginCredentials>>>) {
+        let mut locked_queue = queue.lock().await;
+        locked_queue.is_running = true;
+        drop(locked_queue);
         
-        loop {
-            let mut locked_manager = manager.lock().await;
+        'outer: loop {
+            let mut locked_queue = queue.lock().await;
             
-            if locked_manager.length() > 0 {
-                let mut output = locked_manager.queue.remove(0);
+            if locked_queue.length() > 0 {
+                let mut output = locked_queue.queue.remove(0);
+            
+                let mut to_remove: usize = 0;
+                
+                for item in locked_queue.cache.iter() {
+                    if item.timestamp.elapsed() > Duration::from_secs(30) {
+                        to_remove += 1;
+                    } else if item.text == output.text {
+                        output.text.push('ㅤ');
+                    }
+                }
+                
+                for _ in 0..to_remove {
+                    locked_queue.cache.remove(0);
+                }
+                
+                locked_queue.cache.push(CacheItem::new(output.text.clone()));
+                
                 output.send_in_context_using(Arc::clone(&client)).await.unwrap();
                 
-                locked_manager.last_sent = Instant::now();
+                locked_queue.last_sent = Instant::now();
                 
                 if BotPermissions::Default == output.input.ctx.environment.lock().await.permissions {
                     task::sleep(Duration::from_secs(1)).await;
                 }
             }
-            drop(locked_manager);
+            drop(locked_queue);
             
             task::sleep(Duration::from_millis(10)).await;
         }
@@ -64,6 +98,7 @@ impl Default for OutputQueue {
         OutputQueue {
             queue: vec![],
             last_sent: Instant::now(),
+            cache: vec![],
             is_running: false
         }
     }
